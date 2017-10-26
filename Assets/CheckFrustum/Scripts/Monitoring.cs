31749c713f3e47b083b683b62ad6ad6e729aa0bd
edom18 <edo.m18@gmail.com>
2017-10-26 23:56:58 +0900

    視錐台の視覚化と、マトリクスの生成をカメラに依存しないで生成するように

--- Assets/CheckFrustum/Scripts/Monitoring.cs
+++ Assets/CheckFrustum/Scripts/Monitoring.cs
@@ -30,19 +30,11 @@ public class Monitoring : MonoBehaviour
 
     private float Aspect
     {
-        get
-        {
-            return (float)Screen.width / Screen.height;
-        }
+        get { return (float)Screen.width / Screen.height; }
     }
 
     private void Start()
     {
-        ShowPlanes();
-
-        Matrix4x4 pmat = Camera.main.projectionMatrix;
-        //Matrix4x4 m = Matrix4x4.Perspective(cam.fieldOfView, cam.aspect, cam.nearClipPlane, cam.farClipPlane);
-
         _materials = new Material[_targets.Length];
         for (int i = 0; i < _targets.Length; i++)
         {
@@ -52,9 +44,10 @@ public class Monitoring : MonoBehaviour
 
     private void Update()
     {
+        Matrix4x4 pmat = Matrix4x4.Perspective(_fov, Aspect, _near, _far);
         for (int i = 0; i < _targets.Length; i++)
         {
-            CheckFrustum.State result = CheckFrustum.Detect(_targets[i], Camera.main.projectionMatrix, Camera.main.transform, Camera.main.nearClipPlane, Camera.main.farClipPlane);
+            CheckFrustum.State result = CheckFrustum.Detect(_targets[i], pmat, transform, _near, _far);
             switch (result)
             {
                 case CheckFrustum.State.Outside:
@@ -73,25 +66,6 @@ public class Monitoring : MonoBehaviour
     private void OnDrawGizmos()
     {
         Gizmos.matrix = transform.localToWorldMatrix;
-        Gizmos.DrawFrustum(transform.position, _fov, _far, _near, Aspect);
-        //Gizmos.DrawFrustum(transform.position, _fov, _far, _near, Camera.main.aspect);
-    }
-
-    private void ShowPlanes()
-    {
-        Matrix4x4 pmat = Camera.main.projectionMatrix;
-        //Plane[] planes = GeometryUtility.CalculateFrustumPlanes(Camera.main);
-        Plane[] planes = CheckFrustum.CalculateFrustumPlanes(pmat, Camera.main.transform, Camera.main.nearClipPlane, Camera.main.farClipPlane);
-        int i = 0;
-        while (i < planes.Length)
-        {
-            GameObject p = GameObject.CreatePrimitive(PrimitiveType.Plane);
-            p.name = "Plane " + i.ToString();
-            p.transform.position = -planes[i].normal * planes[i].distance;
-            p.transform.rotation = Quaternion.FromToRotation(Vector3.up, planes[i].normal);
-            i++;
-
-            p.transform.parent = Camera.main.transform;
-        }
+        Gizmos.DrawFrustum(Vector3.zero, _fov, _far, _near, Aspect);
     }
 }
